c80d0e71bae8693e55d9e593373ddcd85a727352
Chikeobi <chikeobi.njaka@gmail.com>
2020-03-19 15:42:18 -0700

    Completed parsing the values in the modal window

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -15,20 +15,102 @@ function greaterThan(input1, input2) {
 }
 /* Global variables
  */
-var passwdLen; // Length of generated password(s)
-var passwdCount; // Number of passwords to generate
-var maxSpecialChar; // maximum number of special characters in each password
+
+const MIN_PASSWORD_LEN = 8;
+const MAX_PASSWORD_LEN = 128;
+var passwdLen = MIN_PASSWORD_LEN; // Length of generated password(s)
+
+const PASSWORD_COUNT = 3;
+const MAX_PASSWORD_COUNT = 5;
+var passwdCount = PASSWORD_COUNT; // Number of passwords to generate
+
+const MAX_SPECIAL_CHAR_COUNT = 8;
+const MIN_SPECIAL_CHAR_COUNT = 2;
+var maxSpecialCharCount = MIN_SPECIAL_CHAR_COUNT; // maximum number of special characters in each password
+
+const SPECIAL_CHARACTERS = "#!*%";
+var specialCharacters = SPECIAL_CHARACTERS;
+
+const ALPHA_NUMERIC_CHARS =
+  "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789";
+
 /**************************************************
     This function is called by the onClick event of 
     the "Generate" button on the modal.
 ***************************************************/
 function generatePassword() {
-  console.log("Generate Password Button Clicked");
-  //    Read the values in the various inputs
+  var docElement, docElements;
+  var value, intValue, stringValue;
+
+  //    Get Characters per password entry. Default is MIN_PASSWORD_LEN
+  if (
+    (docElement = document.getElementById("pwd-len-input")) != null &&
+    (stringValue = docElement.value) != null &&
+    (intValue = Number.parseInt(stringValue)) != NaN &&
+    (intValue >= MIN_PASSWORD_LEN || intValue <= MAX_PASSWORD_LEN)
+  ) {
+    passwdLen = intValue;
+  }
+  // get special characters per password
+
+  if (
+    (docElement = document.getElementById("special-char-count")) != null &&
+    (stringValue = docElement.value) != null &&
+    (intValue = Number.parseInt(stringValue)) != NaN &&
+    (intValue >= MIN_PASSWORD_LEN || intValue <= MAX_PASSWORD_LEN)
+  ) {
+    maxSpecialCharCount = intValue;
+  }
+
+  // check the radio button by name, not ID
+  //
+  if (
+    (docElements = document.getElementsByName("number-of-passwords")) != null
+  ) {
+    for (let index = 0; index < docElements.length; index++) {
+      if (
+        (docElement = docElements[index]) != null &&
+        docElement.checked === true &&
+        (intValue = Number.parseInt(docElement.value)) != NaN
+      ) {
+        passwdCount = intValue;
+      }
+    }
+  }
+
+  // check to see which special characters were choosen
+  // get the "option" tags
+  if (
+    (docElement = document.getElementById("special-char-selection")) != null &&
+    (docElements = document.querySelectorAll("option")) != null &&
+    docElements.length != 0
+  ) {
+    stringValue = "";
+    for (
+      let index = 0;
+      index < docElements.length && index < MAX_SPECIAL_CHAR_COUNT;
+      index++
+    ) {
+      // check if the option is selected. If so, append to stringValue
+      docElement = docElements[index];
+      console.log(docElement);
+      if (docElement.selected) {
+        console.log(docElement + " (" + docElement.value + ")");
+        stringValue += docElement.value;
+      }
+    }
+    console.log(stringValue);
+    if (stringValue.length != 0) {
+      specialCharacters = stringValue;
+    }
+  }
+  console.log("Password Length = " + passwdLen);
+  console.log("Number of Passwords to generate: " + passwdCount);
+  console.log("Max Special Characters = " + maxSpecialCharCount);
+  console.log("Special Characters: " + specialCharacters);
 
   //    The following uses the ID of the modal section of the document to
   //   reference and close it
-
   $(pwdSpecificsModal).modal("hide");
-  console.log("Just closed the Modal");
+  //   console.log("Just closed the Modal");
 }
